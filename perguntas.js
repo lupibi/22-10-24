a1d162540a7d8a985838713d64be3190af875aee
lupibi <138132973+lupibi@users.noreply.github.com>
2024-10-22 09:57:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é uma DNA?',
+    '"É um tipo de ácido nucleico que possui destaque por armazenar a informação genética da grande maioria dos seres vivos"'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz FELIZ ANIVERSÁRIO em Inglês?',
+    'happy birthday'
+    
+)
